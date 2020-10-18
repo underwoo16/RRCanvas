8d415957e50f851f4b124841b2db4bd9b6eec757
Chandler Underwood <underwoo16@up.edu>
2020-10-18 16:04:54 -0700

    initialize colors array in redux store
    and connect the CanvasSquare to store

--- src/components/CanvasSquare.tsx
+++ src/components/CanvasSquare.tsx
@@ -1,16 +1,26 @@
 import React, { useCallback } from 'react';
+import { connect } from 'react-redux';
+import { IRootState } from '../store';
 
 import './CanvasSquare.css';
 
 interface Props {
     row: number,
-    column: number
+    column: number,
 }
-const CanvasSquare = ({ row, column } : Props) => {
+
+const mapStateToProps = ({ canvas }: IRootState, { row, column }: Props) => {
+    const { colors } = canvas;
+    const currentColor = colors[row][column];
+    return { row, column, currentColor };
+};
+type ReduxType = ReturnType<typeof mapStateToProps>;
+
+const CanvasSquare = ({ row, column, currentColor } : ReduxType) => {
     const onSquareClick = useCallback(() => { console.log(`square ${row}x${column} clicked`); }, [row, column]);
     return (
-        <td className='canvas-square' onClick={onSquareClick}></td>
+        <td className='canvas-square' style={{ backgroundColor: currentColor }} onClick={onSquareClick}></td>
     )
 };
 
-export default CanvasSquare;
+export default connect(mapStateToProps)(CanvasSquare);
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,8 +1,24 @@
 import { ICanvasState } from "./types";
 
+const DEFAULT_SIZE = 8;
+const DEFAULT_COLOR = '#FFFFFF';
+
+const intitializeColors = (size: number, color: string) => {
+    let colors: string[][] = [];
+
+    for (let rowIdx = 0; rowIdx < size; rowIdx++) {
+        colors.push([]);
+        for (let colIdx = 0; colIdx < size; colIdx++) {
+            colors[rowIdx].push(color);
+        }
+    }
+
+    return colors;
+};
+
 const initialState: ICanvasState = {
-    colors: [],
-    size: 8
+    colors: intitializeColors(DEFAULT_SIZE, DEFAULT_COLOR),
+    size: DEFAULT_SIZE
 };
 
 const canvasReducer = (state: ICanvasState = initialState, action: any) => {
